2b2ab4a130718fdb1a5e52512346f72a52716f0d
Amrita Biswas <amritabis12@gmail.com>
2025-07-07 18:47:54 -0500

    Solution for friend requests overall acceptance rate

---
+++ solutions/friend_requests_overall_acceptance_rate.sql
@@ -0,0 +1,4 @@
+Select round(IFNULL(
+(select  count(distinct requester_id , accepter_id ) from RequestAccepted) /
+(select  count(distinct sender_id, send_to_id) from FriendRequest), 0), 2)
+as accept_rate
