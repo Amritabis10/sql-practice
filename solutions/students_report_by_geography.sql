dd697e4da6f350586d1d1b4dfd64326715a999f0
Amrita Biswas <amritabis12@gmail.com>
2025-07-13 11:33:48 -0500

    Solution for students report by grography

--- solutions/students_report_by_geography.sql
+++ solutions/students_report_by_geography.sql
@@ -1,5 +1,15 @@
-with cte as ( select name, continent, dense_rank()over (partition by continent order by name) as rn from Student)
-select MAX(case when continent= 'America' then name else Null end) as America,
-MAX(case when continent= 'Asia' then name else null end) as Asia,
-MAX(case when continent= 'Europe' then name else null end) as Europe from cte
-group by rn order by name ;
+SELECT
+  MAX((CASE WHEN continent = 'America' THEN X.name ELSE NULL END)) AS "America",
+  MAX((CASE WHEN continent = 'Asia' THEN X.name ELSE NULL END)) AS "Asia",
+  MAX((CASE WHEN continent = 'Europe' THEN X.name ELSE NULL END)) AS "Europe"
+FROM
+  (
+    SELECT
+      continent,
+      name,
+      ROW_NUMBER() OVER(PARTITION BY continent ORDER BY name) AS rn
+    FROM
+      student
+  ) X
+GROUP BY
+  rn;
