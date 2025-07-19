a6c13b9dba58ca06ca044762f83b83314589ac40
Amrita Biswas <amritabis12@gmail.com>
2025-07-19 11:19:00 -0500

    Solution for game play analysis iv

--- solutions/game_play_analysis_iv.sql
+++ solutions/game_play_analysis_iv.sql
@@ -1,2 +1,6 @@
 -- Game Play Analysis IV
 -- Write your SQL solution here
+SELECT ROUND(COUNT(DISTINCT player_id) / (SELECT COUNT(DISTINCT player_id) FROM Activity), 2) as fraction
+FROM Activity
+WHERE (player_id, DATE_SUB(event_date, INTERVAL 1 DAY))
+IN (SELECT player_id, MIN(event_date) AS first_login FROM ACTIVITY GROUP BY player_id)
