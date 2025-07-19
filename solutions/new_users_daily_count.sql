d84d9123017859112cff7fe5577f1241c235bf44
Amrita Biswas <amritabis12@gmail.com>
2025-07-19 15:33:27 -0500

    Solution for new users daily count

--- solutions/new_users_daily_count.sql
+++ solutions/new_users_daily_count.sql
@@ -1,2 +1,8 @@
 -- Solution for New Users Daily Count problem
 -- Write your SQL solution here
+With cte as (Select user_id, activity_date,
+dense_rank() over (partition by user_id order by activity_date) as rnk from Traffic where activity = 'login'),
+cte2 as ( select user_id , activity_date from cte where rnk = 1 and activity_date>= DATE_SUB('2019-06-30', INTERVAL 90 DAY))
+SELECT 
+  activity_date AS login_date,
+  COUNT(DISTINCT user_id) AS user_count from cte2 group by activity_date
