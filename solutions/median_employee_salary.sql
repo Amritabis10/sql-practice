9502f6fddb8702c26d42e79786443a02e600034d
Amrita Biswas <amritabis12@gmail.com>
2025-07-19 11:50:31 -0500

    Solution  for median employee salary

--- solutions/median_employee_salary.sql
+++ solutions/median_employee_salary.sql
@@ -1,2 +1,8 @@
 -- Solution for Median Employee Salary problem
 -- Write your SQL solution here
+select Id, Company, Salary
+from (select id, company, salary, 
+			 row_number() over(partition by company order by salary) salaryrank, 
+			 count(*) over(partition by company) tte 
+	  from employee) as foo
+where salaryrank >= tte/2 and salaryrank <= tte/2+1
