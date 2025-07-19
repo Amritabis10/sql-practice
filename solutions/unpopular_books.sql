bed646768b96d219164b86c4b2696eba861230e3
Amrita Biswas <amritabis12@gmail.com>
2025-07-19 14:50:42 -0500

    Solution for unpopular books

--- solutions/unpopular_books.sql
+++ solutions/unpopular_books.sql
@@ -1,2 +1,16 @@
 -- Solution for Unpopular Books problem
 -- Write your SQL solution here
+with booklist as(
+    select * 
+    from books
+    where datediff(month,available_from, '2019-06-23')>1
+)
+select b.book_id, b.name
+from (
+    select book_id,sum(quantity) as total
+    from orders
+    where dateadd(year,-1,'2019-6-23')<dispatch_date
+    group by book_id )a
+right join booklist b
+on a.book_id = b.book_id
+where a.total <10 or a.total is null
