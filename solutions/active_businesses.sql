1a2c7804fd315532a430dc176a0b132487005dbb
Amrita Biswas <amritabis12@gmail.com>
2025-07-21 18:51:41 -0500

    Solution for active businesses

--- solutions/active_businesses.sql
+++ solutions/active_businesses.sql
@@ -1,2 +1,24 @@
 -- Active Businesses Solution
 -- Write your SQL solution here
+
+WITH cte AS (
+    SELECT 
+        event_type, 
+        AVG(occurrences) AS avg_oc 
+    FROM Events 
+    GROUP BY event_type
+),
+cte1 AS (
+    SELECT 
+        e.business_id, 
+        e.event_type, 
+        e.occurrences, 
+        c.avg_oc 
+    FROM cte AS c 
+    JOIN Events AS e ON e.event_type = c.event_type 
+    WHERE e.occurrences > c.avg_oc
+)
+SELECT business_id 
+FROM cte1 
+GROUP BY business_id 
+HAVING COUNT(event_type) > 1;
