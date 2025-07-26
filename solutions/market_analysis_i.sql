754226fb81583e33a7cf713a9a215aabdfae985f
Amrita Biswas <amritabis12@gmail.com>
2025-07-26 12:34:56 -0500

    Solution for market analysis i

--- solutions/market_analysis_i.sql
+++ solutions/market_analysis_i.sql
@@ -1,2 +1,7 @@
 -- Market Analysis I
 -- Write your solution here
+Select u.user_id as buyer_id, u.join_date, 
+ifnull(count( o.item_id), 0) as orders_in_2019
+from users as u left join orders as o on 
+u.user_id=o.buyer_id and
+year(o.order_date) = '2019' group by u.user_id
