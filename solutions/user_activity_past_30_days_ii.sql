0953888a33e31a3900b96ac4d3c4c1938d88d069
Amrita Biswas <amritabis12@gmail.com>
2025-07-25 23:29:37 -0500

    Solution for user activity past 30 days ii

--- solutions/user_activity_past_30_days_ii.sql
+++ solutions/user_activity_past_30_days_ii.sql
@@ -1,2 +1,13 @@
 -- User Activity for the Past 30 Days II
 -- Write your solution here
+WITH CTE AS (
+    SELECT 
+        user_id, 
+        COUNT(DISTINCT session_id) AS cnt
+    FROM Activity
+    WHERE activity_date BETWEEN '2019-06-28' AND '2019-07-27'
+    GROUP BY user_id
+)
+SELECT 
+     COALESCE(ROUND(SUM(cnt) * 1.0 / COUNT(*), 2), 0) AS average_sessions_per_user
+FROM CTE;
