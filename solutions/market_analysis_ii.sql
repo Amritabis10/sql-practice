ef9db0498c27067f5b0cdf05186f44cff7b32403
Amrita Biswas <amritabis12@gmail.com>
2025-07-26 15:43:22 -0500

    Solution for market analysis ii

--- solutions/market_analysis_ii.sql
+++ solutions/market_analysis_ii.sql
@@ -1,2 +1,23 @@
 -- Market Analysis II
 -- Write your solution here
+WITH orders_ranked AS (
+  SELECT
+    order_id,
+    order_date,
+    item_id,
+    seller_id,
+    DENSE_RANK() OVER (PARTITION BY seller_id ORDER BY order_date) AS rk
+  FROM Orders
+)
+SELECT
+  u.user_id AS seller_id,
+  CASE
+    WHEN i.item_brand = u.favorite_brand THEN 'yes'
+    ELSE 'no'
+  END AS 2nd_item_fav_brand
+FROM Users u
+LEFT JOIN orders_ranked o
+  ON u.user_id = o.seller_id
+ AND o.rk = 2
+LEFT JOIN Items i
+  ON o.item_id = i.item_id;
