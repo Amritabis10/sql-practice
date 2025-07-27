54b620486c43031d04f4ea26f814e069473d42ad
Amrita Biswas <amritabis12@gmail.com>
2025-07-27 11:00:20 -0500

    Solution for immediate food delivery

--- solutions/immediate_food_delivery_i.sql
+++ solutions/immediate_food_delivery_i.sql
@@ -1,2 +1,5 @@
 -- Immediate Food Delivery I
 -- Write your solution here
+With cte as ( select count(delivery_id) as ct from Delivery where order_date=customer_pref_delivery_date),
+cte2 as(select count(delivery_id)ct1 from Delivery)
+select round((ct/ct1)*100, 2) as immediate_percentage  from cte, cte2
