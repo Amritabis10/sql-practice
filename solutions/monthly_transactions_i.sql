c4cc940e43f3fd0b08e6ccda18c376f9696405eb
Amrita Biswas <amritabis12@gmail.com>
2025-07-27 16:21:46 -0500

    Solution for monthly transaction i

--- solutions/monthly_transactions_i.sql
+++ solutions/monthly_transactions_i.sql
@@ -1,2 +1,13 @@
 -- Monthly Transactions I
 -- Write your solution here
+
+Select date_format(trans_date, '%Y-%m') as month,
+ country, 
+ count(id) as trans_count, 
+sum(case when state = 'approved' then 1 else 0 end) as approved_count, 
+SUM(amount)as trans_total_amount,
+sum(case when state = 'approved' then amount else 0 end) as approved_total_amount
+FROM Transactions GROUP BY date_format(trans_date, '%Y-%m'), country
+
+
+
