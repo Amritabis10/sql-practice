fc65acd7cee89ef66e0cc61b5bac876e07740350
Amrita Biswas <amritabis12@gmail.com>
2025-07-27 15:21:34 -0500

    Solution for immediate food delivery ii

--- solutions/immediate_food_delivery_ii.sql
+++ solutions/immediate_food_delivery_ii.sql
@@ -1,2 +1,14 @@
 -- Immediate Food Delivery II
 -- Write your solution here
+WITH First_Order AS
+(
+SELECT
+delivery_id,customer_id,order_date,customer_pref_delivery_date,
+RANK() OVER(Partition by customer_id ORDER BY order_date ASC) as rk
+FROM
+Delivery
+)
+SELECT
+ROUND((SELECT COUNT(*) FROM First_Order WHERE order_date = customer_pref_delivery_date AND rk =1) * 100 /COUNT(DISTINCT customer_id),2) as immediate_percentage
+FROM
+Delivery
