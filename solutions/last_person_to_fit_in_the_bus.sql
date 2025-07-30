0b24d7db2d39758574d47768d14925b03ac89de2
Amrita Biswas <amritabis12@gmail.com>
2025-07-30 17:44:03 -0500

    Solution forperson to fit in the bus

--- solutions/last_person_to_fit_in_the_bus.sql
+++ solutions/last_person_to_fit_in_the_bus.sql
@@ -1,2 +1,4 @@
 -- Last Person to Fit in the Bus
 -- Write your SQL solution here
+With cte as ( select *, sum(weight)over(order by turn) as max_wt from Queue)
+select person_name from cte where max_wt <= 1000 order by max_wt desc limit 1
