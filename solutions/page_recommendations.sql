e19bf732400d1a2172c72ebb8cf86f87e0e124da
Amrita Biswas <amritabis12@gmail.com>
2025-08-03 11:12:58 -0500

    Solution for page recommendations

--- solutions/page_recommendations.sql
+++ solutions/page_recommendations.sql
@@ -1,2 +1,6 @@
 -- Page Recommendations Solution
 -- Write your SQL solution here
+select distinct page_id as recommended_page 
+from Likes 
+where page_id not in( select page_id from Likes where user_id = 1)
+ and user_id in( select user2_id from Friendship where user1_id = 1 UNION SELECT user1_id from friendship WHERE user2_id = 1)
