e747a6bdfb1651673274eadb9962df9d984737b9
Amrita Biswas <amritabis12@gmail.com>
2025-08-03 15:07:52 -0500

    Solution for all people report to the given manager

--- solutions/all_people_report_to_the_given_manager.sql
+++ solutions/all_people_report_to_the_given_manager.sql
@@ -1,2 +1,12 @@
 -- All People Report to the Given Manager Solution
 -- Write your SQL solution here
+Select distinct e1.employee_id from employees e1
+ left join employees e2 on e1.manager_id = e2.employee_id
+left join employees e3 on e2.manager_id = e3.employee_id
+where
+(
+    e1.manager_id=1
+OR e2.manager_id=1
+or e3.manager_id=1)
+and e1.employee_id<> 1
+
