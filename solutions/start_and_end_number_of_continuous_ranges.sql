b84e337907d58d9fd3de29c60569f93c4f6099d4
Amrita Biswas <amritabis12@gmail.com>
2025-08-12 18:47:19 -0500

    Solution for start and end number of continuous ranges

--- solutions/start_and_end_number_of_continuous_ranges.sql
+++ solutions/start_and_end_number_of_continuous_ranges.sql
@@ -1,2 +1,6 @@
 -- Solution for: Find the Start and End Number of Continuous Ranges
 -- Write your SQL solution here
+
+
+With cte as ( select log_id, rank() over(order by log_id) as rk from Logs)
+Select min(log_id) as start_id, max(log_id) as end_id from cte group by  log_id-rk
