0010b8a0dda187badf39eaf2327979429d0ad781
Amrita Biswas <amritabis12@gmail.com>
2025-08-17 10:39:59 -0500

    Solution for ads performance

--- solutions/ads_performance.sql
+++ solutions/ads_performance.sql
@@ -1,2 +1,4 @@
 -- Ads Performance
 -- Write your solution here
+Select ad_id, IFNULL(round((sum(case when action='clicked' then 1 else 0 end )/ sum(case when action='clicked' or action='viewed' then 1 else 0 end ))*100, 2), 0) as ctr 
+from Ads group by ad_id order by ctr desc, ad_id asc
