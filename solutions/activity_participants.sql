d694c42737b754c6e3dd23bb46c2fc97df621778
Amrita Biswas <amritabis12@gmail.com>
2025-08-20 18:41:48 -0500

    Solution for activity participants

--- solutions/activity_participants.sql
+++ solutions/activity_participants.sql
@@ -1,2 +1,7 @@
 -- Activity Participants
 -- Find the names of all the activities with neither the maximum nor the minimum number of participants.
+With cte as ( select activity, count(distinct id) as cnt from Friends group by activity)
+Select activity from cte where cnt not in( select max(cnt) from cte
+union all
+select min(cnt) from cte
+)
