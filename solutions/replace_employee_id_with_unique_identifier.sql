835bdd9c3724e266f994793a027e55594ed07655
Amrita Biswas <amritabis12@gmail.com>
2025-08-23 10:00:08 -0500

    Solution for replace employee ID with the unique identifier

--- solutions/replace_employee_id_with_unique_identifier.sql
+++ solutions/replace_employee_id_with_unique_identifier.sql
@@ -1,2 +1,5 @@
 -- Replace Employee ID With The Unique Identifier
 -- Write your SQL solution here
+Select u.unique_id as unique_id, e.name from employees
+ as e left join EmployeeUNI as u on
+e.id = u.id
