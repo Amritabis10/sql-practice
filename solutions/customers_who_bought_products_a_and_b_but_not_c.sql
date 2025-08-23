19a3232672817d0db1782c321460a2b31e44c58f
Amrita Biswas <amritabis12@gmail.com>
2025-08-23 11:10:22 -0500

    Solution for Customers who bought products A and B but not C

--- solutions/customers_who_bought_products_a_and_b_but_not_c.sql
+++ solutions/customers_who_bought_products_a_and_b_but_not_c.sql
@@ -1,2 +1,9 @@
 -- Customers Who Bought Products A and B but Not C
 -- Write your SQL solution here
+
+Select customer_id, customer_name from Customers where customer_id in(Select 
+distinct customer_id from Orders where product_name= 'A')
+AND customer_id in(Select 
+distinct customer_id from Orders where product_name= 'B')
+AND customer_id NOT IN (Select 
+distinct customer_id from Orders where product_name= 'C')
