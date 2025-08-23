6de5fff555f7b77e7b285d5006a03624595698ee
Amrita Biswas <amritabis12@gmail.com>
2025-08-23 10:34:23 -0500

    Solution for capital gain/loss

--- solutions/capital_gain_loss.sql
+++ solutions/capital_gain_loss.sql
@@ -1,2 +1,8 @@
 -- Capital Gain/Loss
 -- Write your SQL solution here
+With cte as ( select stock_name, sum(price) as t_buy from
+Stocks where operation = 'Buy' group by stock_name)
+, cte2 as( select stock_name, sum(price) as t_sell from
+Stocks where Operation = 'Sell' group by stock_name)
+Select cte.stock_name, (cte2.t_sell-cte.t_buy) as capital_gain_loss from cte join cte2
+on cte.stock_name=cte2.stock_name
