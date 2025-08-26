549a16a3ab5b1e90edb1d4662976d2ba65c98919
Amrita Biswas <amritabis12@gmail.com>
2025-08-25 19:05:43 -0500

    Solution for evaluate boolean expression

--- solutions/evaluate_boolean_expression.sql
+++ solutions/evaluate_boolean_expression.sql
@@ -1,2 +1,9 @@
 -- Solution for Evaluate Boolean Expression
 -- Write your SQL query here
+Select e.left_operand, e.operator, e.right_operand, (case WHEN e.operator = '<' AND v1.value < v2.value THEN 'true'
+            WHEN e.operator = '=' AND v1.value = v2.value THEN 'true'
+            WHEN e.operator = '>' AND v1.value > v2.value THEN 'true'
+            ELSE 'false'end) as value from Expressions as e left join Variables as v1 on 
+e.left_operand = v1.name 
+left join Variables as v2 
+ON e.right_operand = v2.name
