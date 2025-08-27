e753ce283f6321a00641c2908319aa7f1a1f1b69
Amrita Biswas <amritabis12@gmail.com>
2025-08-26 19:01:15 -0500

    Solution for apples and oranges

--- solutions/apples_and_oranges.sql
+++ solutions/apples_and_oranges.sql
@@ -1,2 +1,15 @@
 -- Solution for Apples & Oranges problem
 -- Write your SQL solution here
+WITH cte AS (
+    SELECT 
+        sale_date,
+        SUM(CASE WHEN fruit = 'apples' THEN sold_num ELSE 0 END) AS apple,
+        SUM(CASE WHEN fruit = 'oranges' THEN sold_num ELSE 0 END) AS orange
+    FROM Sales
+    GROUP BY sale_date
+)
+SELECT 
+    sale_date,
+    (apple - orange) AS diff
+FROM cte
+ORDER BY sale_date;
