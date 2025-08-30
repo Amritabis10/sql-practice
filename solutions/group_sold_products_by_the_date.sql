8ba9ada206905ab193bc0e310fab0157bdf3e090
Amrita Biswas <amritabis12@gmail.com>
2025-08-30 10:43:18 -0500

    Solution for group sold product by the date

--- solutions/group_sold_products_by_the_date.sql
+++ solutions/group_sold_products_by_the_date.sql
@@ -1,2 +1,3 @@
 -- Group Sold Products By The Date
 -- Write your solution here
+Select sell_date, count(distinct product) as num_sold, GROUP_CONCAT( DISTINCT product order by product  asc separator ',') as products from Activities group by sell_date order by sell_date
