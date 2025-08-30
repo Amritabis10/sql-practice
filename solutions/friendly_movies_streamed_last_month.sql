d2aea2eb6475b3cf81880b42ebda00951a677270
Amrita Biswas <amritabis12@gmail.com>
2025-08-30 16:43:26 -0500

    Solution for friendly movies streamed

--- solutions/friendly_movies_streamed_last_month.sql
+++ solutions/friendly_movies_streamed_last_month.sql
@@ -1,2 +1,6 @@
 -- Friendly Movies Streamed Last Month
 -- Write your solution here
+Select distinct c.title  from content as c join 
+TVProgram as t on t.content_id= c.content_id
+and Left(program_date, 7) = '2020-06'
+where Kids_content = 'Y' and content_type = 'Movies'
