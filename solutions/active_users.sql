0d9c8f1c87e14540471b9dd124e8f79ced47b6c7
Amrita Biswas <amritabis12@gmail.com>
2025-08-29 19:19:25 -0500

    Solution for active users

--- solutions/active_users.sql
+++ solutions/active_users.sql
@@ -1,2 +1,23 @@
 -- Active Users
 -- Write your solution here
+
+
+WITH cte AS (
+    SELECT DISTINCT id, login_date
+    FROM Logins
+),
+cte2 AS (
+    SELECT 
+        id,
+        DATEDIFF(
+            LEAD(login_date, 4) OVER (PARTITION BY id ORDER BY login_date), 
+            login_date
+        ) AS gap
+    FROM cte
+)
+SELECT DISTINCT cte2.id, a.name
+FROM cte2
+JOIN Accounts a ON cte2.id = a.id
+WHERE cte2.gap = 4
+ORDER BY cte2.id;
+
