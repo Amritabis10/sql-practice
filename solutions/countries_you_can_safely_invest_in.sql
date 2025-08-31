039f7124de690db0ad6c38bc01597ff027f11a80
Amrita Biswas <amritabis12@gmail.com>
2025-08-31 11:34:24 -0500

    Solution for countries you can safetly invest in

--- solutions/countries_you_can_safely_invest_in.sql
+++ solutions/countries_you_can_safely_invest_in.sql
@@ -1,2 +1,16 @@
 -- Countries You Can Safely Invest In
 -- Write your solution here
+with base as
+(
+    select caller_id as person_id, duration from Calls
+    union all
+    select callee_id as person_id, duration from Calls
+)
+
+select
+    c.name as country
+from base as b
+join Person as p on b.person_id = p.id
+join Country as c on substr(p.phone_number, 1, 3) = c.country_code
+group by c.name
+having avg(b.duration) > (select avg(duration) from Calls)
