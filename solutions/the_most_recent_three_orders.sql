45f3d3e6f62f9bb572380125dd0b60b423a24e36
Amrita Biswas <amritabis12@gmail.com>
2025-09-02 19:07:15 -0500

    Solution for the most recent 3 orders

--- solutions/the_most_recent_three_orders.sql
+++ solutions/the_most_recent_three_orders.sql
@@ -1,2 +1,8 @@
 -- Solution for: The Most Recent Three Orders
 -- Write your SQL solution here
+Select  name as customer_name, customer_id, order_id, order_date from
+(Select c.name, o.customer_id, o.order_id, o.order_date, dense_rank() over
+(partition by o.customer_id order by o.order_date desc) as rnk 
+from orders as o 
+join customers as c on o.customer_id=c.customer_id) as t
+where rnk <= 3 ORDER BY customer_name, customer_id, order_date DESC;
