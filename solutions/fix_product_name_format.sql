63cc3da9526bb80dc9ad3d114e1c242ce72a14e3
Amrita Biswas <amritabis12@gmail.com>
2025-09-02 23:05:11 -0500

    Solution for fix product name format

--- solutions/fix_product_name_format.sql
+++ solutions/fix_product_name_format.sql
@@ -1,2 +1,6 @@
 -- Solution for: Fix Product Name Format
 -- Write your SQL solution here
+Select  trim(lower(product_name)) as product_name, left(sale_date, 7) as sale_date,
+count(sale_id) as total from Sales group by trim(lower(product_name)), 
+left(sale_date, 7) 
+order by product_name, sale_date
