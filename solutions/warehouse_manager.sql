01853737451e84a44696538d87a6c83acac07a9a
Amrita Biswas <amritabis12@gmail.com>
2025-09-07 10:50:02 -0500

    Solution for warehouse manager

--- solutions/warehouse_manager.sql
+++ solutions/warehouse_manager.sql
@@ -1,2 +1,9 @@
 -- Solution for Warehouse Manager problem
 -- Write your SQL solution here
+SELECT 
+    w.name AS warehouse_name,
+    SUM(w.units * (p.Width * p.Length * p.Height)) AS volume
+FROM Warehouse w
+JOIN Products p
+    ON w.product_id = p.product_id
+GROUP BY w.name;
