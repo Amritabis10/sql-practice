4a4e52838c6b5840458caf6221268b32e08a2ba2
Amrita Biswas <amritabis12@gmail.com>
2025-09-07 11:36:28 -0500

    Solution for customer who visited but but did not make any transactions

--- solutions/customer_who_visited_but_did_not_make_any_transactions.sql
+++ solutions/customer_who_visited_but_did_not_make_any_transactions.sql
@@ -1,2 +1,8 @@
 -- Solution for Customer Who Visited but Did Not Make Any Transactions problem
 -- Write your SQL solution here
+With cte as 
+(Select visit_id, customer_id from Visits where
+ visit_id not in(Select visit_id from
+Transactions)) 
+Select customer_id, count(distinct visit_id) as count_no_trans from
+cte  group by customer_id
