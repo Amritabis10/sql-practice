180ac43fc2d2eb6cad5daa52698b4c75fd111bdb
Amrita Biswas <amritabis12@gmail.com>
2025-09-09 19:06:41 -0500

    Solution for bank account  summary ii

--- solutions/bank_account_summary_ii.sql
+++ solutions/bank_account_summary_ii.sql
@@ -1,2 +1,5 @@
 -- Bank Account Summary II
 -- Write your SQL solution here
+Select u.name, sum(t.amount) as balance from users as u join transactions as t
+on u.account= t.account
+group by u.account having sum(t.amount) > 10000
