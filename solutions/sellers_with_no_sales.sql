40c87dd05468e5ad877e46e4102c3ee7131ecc0d
Amrita Biswas <amritabis12@gmail.com>
2025-09-11 23:28:50 -0500

    Solution for sellers with no sales

--- solutions/sellers_with_no_sales.sql
+++ solutions/sellers_with_no_sales.sql
@@ -1,2 +1,7 @@
 -- Solution for Sellers With No Sales
 -- Write your SQL solution here
+Select seller_name as SELLER_NAME from seller where
+ seller_id 
+not in( select seller_id from Orders where 
+LEFT(sale_date, 4) = 2020) 
+order by 1
