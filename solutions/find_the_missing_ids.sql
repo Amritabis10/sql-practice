702c0cdebe0e258df2fbe40c8b21f087e71ed562
Amrita Biswas <amritabis12@gmail.com>
2025-09-13 11:36:31 -0500

    Solution for find the missing ids

--- solutions/find_the_missing_ids.sql
+++ solutions/find_the_missing_ids.sql
@@ -1,2 +1,33 @@
 -- Find the Missing IDs
 -- Write your solution here
+
+#1. 
+WITH RECURSIVE cte AS (
+    SELECT MAX(customer_id) AS max_id 
+    FROM Customers
+),
+cte1 AS (
+    SELECT 1 AS id
+    UNION ALL
+    SELECT id + 1
+    FROM cte1, cte
+    WHERE id < cte.max_id
+)
+SELECT id AS ids
+FROM cte1
+WHERE id NOT IN (SELECT customer_id FROM Customers)
+ORDER BY id;
+
+
+#2.
+WITH RECURSIVE nums AS (
+    SELECT 1 AS id
+    UNION ALL
+    SELECT id + 1
+    FROM nums, (SELECT MAX(customer_id) AS max_id FROM Customers) m
+    WHERE id < m.max_id
+)
+SELECT id
+FROM nums
+WHERE id NOT IN (SELECT customer_id FROM Customers)
+ORDER BY id;
