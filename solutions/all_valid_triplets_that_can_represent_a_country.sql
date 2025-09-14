b8739d19ae822b9caac3990f7ce7aa7df734419b
Amrita Biswas <amritabis12@gmail.com>
2025-09-14 11:35:49 -0500

    Solution for all valid triplets that can represent a   country

--- solutions/all_valid_triplets_that_can_represent_a_country.sql
+++ solutions/all_valid_triplets_that_can_represent_a_country.sql
@@ -1,2 +1,13 @@
 -- All Valid Triplets That Can Represent a Country
 -- Write your solution here
+Select a.student_name as member_A, 
+b.student_name as member_B,
+c.student_name as member_C
+from SchoolA as a join SchoolB as b on
+a.student_id <> b.student_id
+and a.student_name <> b.student_name
+join SchoolC AS c on
+a.student_id <> c.student_id and
+b.student_id <> c.student_id
+and a.student_name <> c.student_name
+and b.student_name <> c.student_name
