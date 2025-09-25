6224a2e8f903deab181d7ff9e029ca2975a68445
Amrita Biswas <amritabis12@gmail.com>
2025-09-24 19:58:11 -0500

    Solution for product's worth over invoices

--- solutions/products_worth_over_invoices.sql
+++ solutions/products_worth_over_invoices.sql
@@ -1,1 +1,6 @@
 -- Write your solution here
+Select p.name as name , ifnull(sum(i.rest),0) as rest, ifnull(sum(i.paid), 0) as paid, ifnull(sum(i.canceled),0) as canceled, ifnull(sum(i.refunded),0) as refunded
+from product as p left join invoice as i on
+p.product_id = i.product_id
+ group by p.product_id
+ order by p.name
