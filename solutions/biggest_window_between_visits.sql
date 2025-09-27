4a059a06df68ab9146d66cb8e9670bfde189dcbf
Amrita Biswas <amritabis12@gmail.com>
2025-09-27 15:18:47 -0500

    Solution for biggest window between visits

--- solutions/biggest_window_between_visits.sql
+++ solutions/biggest_window_between_visits.sql
@@ -1,1 +1,11 @@
 -- Write your solution here
+with cte1 as
+(select user_id, abs(DATEDIFF(visit_date,visit_date1)) as date_diff
+from (
+select user_id, visit_date,
+ifnull(lead(visit_date,1) over (partition by user_id order by visit_date asc),'2021-1-1') as visit_date1
+from UserVisits) as temp)
+
+select user_id, max(date_diff) as biggest_window
+from cte1
+group by user_id order by user_id
