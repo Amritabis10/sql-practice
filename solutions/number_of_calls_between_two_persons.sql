80d44323289933acb2f8ddc13f8f9af05907c4ac
Amrita Biswas <amritabis12@gmail.com>
2025-09-27 15:01:56 -0500

    Solution for number of calls between 2 persons

--- solutions/number_of_calls_between_two_persons.sql
+++ solutions/number_of_calls_between_two_persons.sql
@@ -1,1 +1,6 @@
 -- Write your solution here
+WITH CTE AS (select least(from_id, to_id) as person1,
+ greatest(from_id, to_id) as person2, duration from
+Calls)
+Select person1, person2, count(*) as call_count, sum(duration) as total_duration
+from cte group by 1,2
