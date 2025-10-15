7d8559eda8b864c4c522075dbc43df422c6d3143
Amrita Biswas <amritabis12@gmail.com>
2025-10-14 19:07:31 -0500

    Solution for total time spent by each employee

--- solutions/find_total_time_spent_by_each_employee.sql
+++ solutions/find_total_time_spent_by_each_employee.sql
@@ -1,1 +1,4 @@
 -- Write your solution here
+With cte as (Select emp_id, event_day, (out_time-in_time) as t_time from Employees)
+Select  event_day as day,emp_id, sum(t_time) as total_time from cte group by emp_id,
+event_day order by day
