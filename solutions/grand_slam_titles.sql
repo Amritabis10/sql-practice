f597359d0d91394ab40120ed710167a594811118
Amrita Biswas <amritabis12@gmail.com>
2025-10-19 12:58:57 -0500

    Solution for grand slam titles

--- solutions/grand_slam_titles.sql
+++ solutions/grand_slam_titles.sql
@@ -1,1 +1,13 @@
 -- Write your solution here
+With cte as ( select year, Wimbledon as player_id from Championships
+Union all 
+select year, Fr_open as player_id from Championships
+Union all 
+select year, US_open as player_id from Championships
+Union all 
+select year, Au_open as player_id from Championships
+)
+Select p1.player_id, p2.player_name, count(p1.player_id) as grand_slams_count
+from cte as p1 join Players as p2
+on p1.player_id = p2.player_id
+group by p1.player_id, p2.player_name 
