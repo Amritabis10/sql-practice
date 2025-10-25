24016e9591a562ef2450e5065dc12aa96700f0f2
Amrita Biswas <amritabis12@gmail.com>
2025-10-25 15:43:59 -0500

    Solution for maximum transaction each day

--- solutions/maximum_transaction_each_day.sql
+++ solutions/maximum_transaction_each_day.sql
@@ -1,1 +1,5 @@
 -- Write your SQL solution here
+with cte as(Select transaction_id, date(day) as t_day, 
+amount, dense_rank()over(
+    partition by date(day) order by amount desc) as rnk from Transactions)
+    Select transaction_id from cte where rnk = 1 order by transaction_id
